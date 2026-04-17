11c781ec04ee4b991d016ef2852c3d72cb1d777e
Gonzalo Fernandes <goncalopesfernandes@gmail.com>
2026-04-17 16:51:12 +0100

    update ex3.py

--- exercicios/ex3.py
+++ exercicios/ex3.py
@@ -10,9 +10,9 @@ num2 = float(num2_str)
 
 # Comparação e saída
 if num1 > num2:
-    print(f"O primeiro número ({num1}) é maior que o segundo ({num2}).")
+    print(f"O primeiro número {num1} é maior que o segundo {num2}.")
 elif num2 > num1:
-    print(f"O segundo número ({num2}) é maior que o primeiro ({num1}).")
+    print(f"O segundo número {num2} é maior que o primeiro {num1}.")
 else:
     print(f"Os números são iguais ({num1}).")
     
