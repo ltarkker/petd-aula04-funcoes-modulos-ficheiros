a7bc201c3c5d05568c9bcc09c465debb46043fa6
Gonzalo Fernandes <goncalopesfernandes@gmail.com>
2026-04-24 14:52:44 +0100

    add more exercises day 24.04.26

---
+++ exercicios/11-ex3.py
@@ -0,0 +1,16 @@
+# Comparação de números
+
+# Entrada de dados
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+
+# Comparação e saída
+if num1 > num2:
+    print(f"O primeiro número {num1} é maior que o segundo {num2}.")
+elif num2 > num1:
+    print(f"O segundo número {num2} é maior que o primeiro {num1}.")
+else:
+    print(f"O número {num1} é igual ao número {num2}.")
+
+
+
